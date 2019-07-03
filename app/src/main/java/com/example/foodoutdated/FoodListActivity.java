7aa128c6c4c8465f175e8be4c1159d199a153667
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-07-03 15:25:37 +0700

    add permission request when load app

--- app/src/main/java/com/example/foodoutdated/FoodListActivity.java
+++ app/src/main/java/com/example/foodoutdated/FoodListActivity.java
@@ -1,10 +1,15 @@
 package com.example.foodoutdated;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,6 +27,13 @@ public class FoodListActivity extends AppCompatActivity {
 
     private List<Product> myDataset = new ArrayList<>();
     private static final int ADD_PRODUCT_REQUEST = 1;
+    private static final int PERMISSION_REQUEST = 484;
+    String[] permissions = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,10 +67,17 @@ public class FoodListActivity extends AppCompatActivity {
 
         mAdapter = new MyAdapter(myDataset);
         recyclerView.setAdapter(mAdapter);
+
+        if (! checkPermissionGranted())
+        {
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST);
+        }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    @Nullable Intent data) {
         if (requestCode == ADD_PRODUCT_REQUEST){
             if (resultCode == RESULT_OK){
                 Product newProduct = null;
@@ -72,6 +91,41 @@ public class FoodListActivity extends AppCompatActivity {
                 }
 
             }
+        }
+    }
+
+    private boolean checkPermissionGranted(){
+
+        boolean arePermissionsGranted = true;
+        for (String permission: permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED)
+            {
+                arePermissionsGranted =false;
+            }
+        }
+
+        return arePermissionsGranted;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != PERMISSION_REQUEST)
+            return;
+
+        boolean arePermissionsGranted = true;
+        for (int grantResult: grantResults) {
+            if (grantResult != PackageManager.PERMISSION_GRANTED)
+            {
+                arePermissionsGranted = false;
+            }
+        }
+
+        if (! arePermissionsGranted && Build.VERSION.SDK_INT >= 23)
+        {
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST);
         }
     }
 }
