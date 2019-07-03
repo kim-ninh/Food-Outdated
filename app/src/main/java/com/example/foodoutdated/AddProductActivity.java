0d2585416dba7b32768548195f152d34ef804b6f
Kim Ninh Huynh <kimninh1610@gmail.com>
2019-07-03 15:40:21 +0700

    implement take picture and chose from gallery

--- app/src/main/java/com/example/foodoutdated/AddProductActivity.java
+++ app/src/main/java/com/example/foodoutdated/AddProductActivity.java
@@ -1,8 +1,15 @@
 package com.example.foodoutdated;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +20,11 @@ import android.widget.Toast;
 import com.example.foodoutdated.custom.view.DateEditText;
 import com.example.foodoutdated.custom.view.ImageHolderView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AddProductActivity extends AppCompatActivity
         implements View.OnClickListener {
 
@@ -21,7 +33,10 @@ public class AddProductActivity extends AppCompatActivity
     private ImageHolderView productImageHolder;
     private Button btnCamera;
     private Button btnGallery;
+    private Uri photoURI;
 
+    private final int TAKE_PICTURE_REQUEST = 111;
+    private final int OPEN_GALLERY_REQUEST = 222;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,9 +78,69 @@ public class AddProductActivity extends AppCompatActivity
     public void onClick(View v) {
         if (v == btnCamera)
         {
-            Toast.makeText(this, "Camera click", Toast.LENGTH_LONG).show();
+            dispatchTakePictureIntent();
         }else if (v == btnGallery){
-            Toast.makeText(this, "Gallery click", Toast.LENGTH_LONG).show();
+            openGallery();
         }
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("CreateImageFile", ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.ninh.foodoutdated",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST);
+            }
+        }
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, OPEN_GALLERY_REQUEST);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+       if (requestCode == OPEN_GALLERY_REQUEST && resultCode == RESULT_OK){
+           Uri imageUri = Uri.parse(data.getDataString());
+           String str = imageUri.getPath();
+           productImageHolder.setImageUri(imageUri);
+       }
+
+       if (requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK){
+           productImageHolder.setImageUri(photoURI);
+       }
+
+    }
+
 }
